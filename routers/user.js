20992fa0d60d7297054094e3010714af2b8318ae
shehan <shehan8998@gmail.com>
2025-04-19 09:18:47 +0530

    delete users

--- routers/user.js
+++ routers/user.js
@@ -23,5 +23,18 @@ router.post('/', async(req, res) => {
     }
      
 })
+router.delete('/:id', async(req, res) => {
+    const { id } = req.params;
+    try{
+        const deletedUser = await User.findByIdAndDelete(id);
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'User deleted successfully', user: deletedUser });
+    }
+    catch(err){
+        res.status(500).json({message: 'Error deleting user', error: err.message});
+    }
+});    
 
 module.exports = router;
